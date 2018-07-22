485547cdf452b186b6604353bfebf33b47d7c242
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2018-07-22 18:50:18 +0200

    cambiar default path

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const basePath = process.env.BASE_PATH  || '.';
+const basePath = process.env.BASE_PATH  || '/Users/Fran/Documents/github/tiger-sleepy';
 
 module.exports = { basePath };
