0d0be628cceb30e31c41f677f31b9c05596287bc
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2018-09-18 00:15:39 +0200

    fix: filter hidden files

--- src/plugins/films/handlers.js
+++ src/plugins/films/handlers.js
@@ -2,13 +2,14 @@
 const fs = require('fs');
 const Boom = require('boom');
 const { assign } = require('lodash');
-const { toEither, pipe, map, filter, compose: B, Left } = require('sanctuary');
+const { toEither, pipe, map, reject, compose: B, splitOn, last, maybe } = require('sanctuary');
 const { basePath } = require('../../config/index');
 const { startVideo, stopVideo } = require('./helper');
 
-const FilterHideFiles = (file) => /^[^.].*/.test(file);  //No tengo que dejar pasar nada que empiece por .
+// const FilterHideFiles = (file) => /.*[^.].*/.test(file);  //No tengo que dejar pasar nada que empiece por .
+const FilterHideFiles = pipe([splitOn('/'), last, maybe(false)((str) => /^[[.].*/.test(str))]);  //No tengo que dejar pasar nada que empiece por .
 const resolveBasePath = (dirPath) => dirPath ? `${basePath}/${dirPath}` : basePath;
-const pretiffyNames = (names) => names.map(name => ({ 'original': name, 'pretty': name }));
+const pretiffyNames = (names) => names.map(name => ({ 'original': name, 'pretty': pipe([splitOn('/'), last])(name) }));
 const addDirectory = (files) => files.map(file => assign(file, { isDirectory: fs.lstatSync(file.original).isDirectory() }));
 const readDir = (path) => {
   try {
@@ -18,6 +19,10 @@ const readDir = (path) => {
   }
 };
 const scanDir = B(readDir)(resolveBasePath);
+const trace = label => x => {
+  console.log(label, x);
+  return x;
+};
 
 const Play = {
   execute(path, video) {
@@ -39,7 +44,7 @@ const actions = [Play, Stop];
 const readFilmsInDir = pipe([
   scanDir,
   toEither(Boom.conflict('Error reading the path')),
-  map(filter(FilterHideFiles)),
+  map(reject(FilterHideFiles)),
   map(pretiffyNames),
   map(addDirectory),
 ]);
